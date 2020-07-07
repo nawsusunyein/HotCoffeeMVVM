ab4c32bdab9be46b28beabba41e138874f75dac2
Naw Su Su Nyein <nawsusunyein@gmail.com>
2020-07-07 10:55:24 +0630

    create empty project

---
+++ HotCoffee/HotCoffee/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  HotCoffee
+//
+//  Created by Naw Su Su Nyein on 7/7/20.
+//  Copyright © 2020 Naw Su Su Nyein. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
